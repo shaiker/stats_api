1d53b4b3ab019f7738fd9d5dcc5c5a5eac5feb24
shaiker <shai.kerer@gmail.com>
2017-08-17 15:48:44 +0300

    added routes for api_controller and for /ping

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,7 +1,8 @@
 class ApiController < ApplicationController
   
   def update_matches
-    system "#{Rails.root}/bundle exec rake update_epl --trace 2>&1 >> #{Rails.root}/log/rake.log &"
+    system "cd #{Rails.root} & bundle exec rake update_epl --trace 2>&1 >> #{Rails.root}/log/rake.log &"
+    render nothing: true 
   end
   
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def ping
+    render text: 'pong'
+  end
 end
